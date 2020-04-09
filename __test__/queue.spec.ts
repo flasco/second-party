18d32f7a0c7eced1eeb03233dfc9ace76fddc5ad
czq <chenzhengqiang@youzan.com>
2020-04-09 16:24:37 +0800

    fix: 添加测试

--- __test__/queue.spec.ts
+++ __test__/queue.spec.ts
@@ -1,9 +1,24 @@
 import queue from '../src/queue';
+import { delay } from '../src/utils';
 
-test('test1', async () => {
+test('test kill', (done) => {
   const x = new queue(5);
   x.work = async (i) => {
-    expect(i).toBe('2');
+    await delay(Math.round(Math.random() * 3000));
+    console.log('action!2', i);
+  };
+
+  for (let i = 0; i < 50; i++) {
+    x.push(i);
   }
-  x.push('1');
-})
+
+  x.drain = () => {
+    console.log('done');
+    done();
+  };
+
+  setTimeout(() => {
+    console.log('kill');
+    x.kill();
+  }, 5000);
+}, 9999999);
--- src/queue/index.ts
+++ src/queue/index.ts
@@ -7,40 +7,40 @@ interface IPayload<T> {
 }
 
 class Queue<T = any> {
-  _id = 0;
-  _preArr: IPayload<T>[] = [];
-  _workArr: IPayload<T>[] = [];
-  _inProcess = 0;
-  _concurrent: number;
+  #id = 0;
+  #preArr: IPayload<T>[] = [];
+  #workArr: IPayload<T>[] = [];
+  #inProcess = 0;
+  #concurrent: number;
 
   constructor(concurrent = 5) {
-    this._concurrent = concurrent;
+    this.#concurrent = concurrent;
   }
 
   _workPush() {
-    while (this._preArr.length > 0 && this._workArr.length < this._concurrent) {
-      const item = this._preArr.shift() as IPayload<T>;
-      this._workArr.push(item);
+    while (this.#preArr.length > 0 && this.#workArr.length < this.#concurrent) {
+      const item = this.#preArr.shift() as IPayload<T>;
+      this.#workArr.push(item);
     }
 
-    const totalLen = this._workArr.length + this._preArr.length;
+    const totalLen = this.#workArr.length + this.#preArr.length;
 
-    if (totalLen < 1 && this._inProcess < 1) this.drain();
+    if (totalLen < 1 && this.#inProcess < 1) this.drain();
     else this._workRun();
   }
 
   async _workRun() {
-    const workLen = this._workArr.length;
+    const workLen = this.#workArr.length;
 
     for (let i = 0; i < workLen; i++) {
-      const current = this._workArr[i];
+      const current = this.#workArr[i];
       if (current && current.isWorking !== 1) {
         current.isWorking = 1;
-        this._inProcess++;
+        this.#inProcess++;
         await this.work(current.content);
-        this._inProcess--;
-        const pos = this._workArr.findIndex((val) => current.id === val.id);
-        this._workArr.splice(pos, 1);
+        const pos = this.#workArr.findIndex((val) => current.id === val.id);
+        this.#workArr.splice(pos, 1);
+        this.#inProcess--;
 
         this._workPush();
       }
@@ -52,16 +52,16 @@ class Queue<T = any> {
   }
 
   push(item) {
-    this._preArr.push({
-      id: this._id++,
+    this.#preArr.push({
+      id: this.#id++,
       content: item,
     });
-    this._workArr.length < this._concurrent && this._workPush();
+    this.#workArr.length < this.#concurrent && this._workPush();
   }
 
   kill() {
-    this._workArr = [];
-    this._preArr = [];
+    this.#workArr = [];
+    this.#preArr = [];
   }
 
   async work(item) {
